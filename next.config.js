b96e966fce44718b33b0014a5232545520c4f25c
Toygun Tez <109132154+Zireaelst@users.noreply.github.com>
2025-06-18 17:46:52 +0300

    feat: Update .gitignore and add vercel.json for deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['ipfs.io', 'gateway.pinata.cloud'],
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+  trailingSlash: true,
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
